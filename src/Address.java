ddab3582c237d9e4f8107a929cbd2562cbcc04f1
aaronl2026 <aaronl2026@student.cis.edu.hk>
2024-02-06 11:19:48 +0800

    new edits

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,19 @@
 public class Address {
+    private String district;
+    private String street;
+    private int streetNumber;
+    public Address(String district, String street, int streetNumber){
+        this.district = district;
+        this.street = street;
+        this.streetNumber = streetNumber;
+    }
+    public String getDistrict() {return district;}
+    public String getStreet() {return street;}
+    public int getStreetNumber() {return streetNumber;}
+    public void setDistrict(String x) {district = x;}
+    public void setStreet(String x) {street = x;}
+    public void setStreetNumber(int x) {streetNumber = x;}
+    public String toString(){
+        return(streetNumber+" "+street+", "+district);
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,34 @@
 public class Student {
+    private int age;
+    private String firstName;
+    private String lastName;
+    private int year;
+    private Address address;
+    public Student(int age, String firstName, String lastName, int year, Address address){
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.address = address;
+    }
+    public int getAge() {return age;}
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public int getYear() {return year;}
+    public Address getAddress() {return address;}
+    public void setAge(int x) {age = x;}
+    public void setFirstName(String x) {firstName = x;}
+    public void setLastName(String x) {lastName = x;}
+    public void setYear(int x) {year = x;}
+    public void setAddress(Address x) {address = x;}
+    private void addOneAge(){
+        age = age + 1;
+    }
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+    public String toString(){
+        return(firstName+" "+lastName+", age "+age+", year "+year+", "+address);
+    }
 }
